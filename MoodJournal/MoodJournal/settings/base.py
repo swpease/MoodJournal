c99c7b1bb000c988bed32f29036ac7908936ed0a
swpease <swpease@gmail.com>
2017-10-29 14:03:58 -0700

    postgres linked up; time zone; installed_apps

--- MoodJournal/MoodJournal/settings/base.py
+++ MoodJournal/MoodJournal/settings/base.py
@@ -40,6 +40,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'entries.apps.EntriesConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -114,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
--- MoodJournal/MoodJournal/settings/development.py
+++ MoodJournal/MoodJournal/settings/development.py
@@ -2,3 +2,14 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_variable('DB_NAME'),
+        'USER': get_env_variable('DB_USER'),
+        'PASSWORD': get_env_variable('DB_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
