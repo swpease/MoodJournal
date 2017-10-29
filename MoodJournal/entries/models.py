1e3cfc00701ea9eb244a583c66cfb315e77d5266
swpease <swpease@gmail.com>
2017-10-29 15:42:45 -0700

    created models and first migration

--- MoodJournal/entries/models.py
+++ MoodJournal/entries/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserDefinedCategory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
+
+class EntryInstance(models.Model):
+    ONE = 'Terrible'
+    TWO = 'Bad'
+    THREE = 'OK'
+    FOUR = 'Good'
+    FIVE = 'Excellent'
+    QUALITY_RATING_CHOICES = (
+        (ONE, 'Terrible'),
+        (TWO, 'Bad'),
+        (THREE, 'OK'),
+        (FOUR, 'Good'),
+        (FIVE, 'Excellent'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(UserDefinedCategory, on_delete=models.CASCADE, unique_for_date="date")
+
+    date = models.DateField()
+    entry = models.TextField()
+    quality_rating = models.CharField(max_length=15, choices=QUALITY_RATING_CHOICES, default=THREE)
+
+    def __str__(self):
+        return self.user, self.date, self.category
