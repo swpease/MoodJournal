67be43846544f81840982efd6150bc2724b7807d
swpease <swpease@gmail.com>
2017-12-05 16:57:05 -0800

    created Entries view

--- MoodJournal/entries/views.py
+++ MoodJournal/entries/views.py
@@ -1,2 +1,17 @@
 from django.shortcuts import render
 
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from .serializers import UserDefinedCategorySerializer, EntryInstanceSerializer
+from .models import UserDefinedCategory, EntryInstance
+
+
+class Entries(generics.ListAPIView):
+    serializer_class = EntryInstanceSerializer
+
+    def get_queryset(self):
+        return Entries.objects.all().filter(user=self.request.user)
+
