5f21ab98ec1e188bab1d88f890400835e834472e
swpease <swpease@gmail.com>
2017-12-07 21:35:45 -0800

    EntriesOnDateList view GET
    got the GET for this view setup to send stuff properly I think. Still a
    little work to do.

--- MoodJournal/entries/urls.py
+++ MoodJournal/entries/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     url('^entries/$', views.EntriesList.as_view(), name='entries-list'),
-
+    url('^entries/(?P<date>[0-9]{8})/$', views.EntriesOnDateList.as_view(), name='entries-on-date'),
 ]
--- MoodJournal/entries/views.py
+++ MoodJournal/entries/views.py
@@ -19,36 +19,45 @@ class EntriesList(generics.ListAPIView):
     #TODO permissions | pagination
 
 
-# class EntriesOnDateList(mixins.ListModelMixin,
-#                         mixins.CreateModelMixin,
-#                         mixins.UpdateModelMixin,
-#                         mixins.DestroyModelMixin,
-#                         generics.GenericAPIView):
-#     """
-#     Provides the set of entries that a User has created on a given date.
-#     HTTP Methods
-#         GET     : All `EntryInstance`s of a User on the date specified by the URL pattern.
-#         POST    : Create a new `EntryInstance` on the given date.
-#         DELETE  : Delete an `EntryInstance` on the given date.
-#         PATCH   : Update an `EntryInstance` on the given date.
-#     """
-#     serializer_class = EntryInstanceSerializer
-#
-#     def get_queryset(self):
-#         # A Django DateField is represented in Python as a datetime.date instance.
-#         # http://www.django-rest-framework.org/api-guide/filtering/  : "Filtering agaisnt the URL"
-#         date_str = self.kwargs['date']  # In the format: YYYYMMDD
-#         date = datetime.date(int(date_str[:4]), int(date_str[4:6]), int(date_str[6:]))
-#
-#         return EntryInstance.objects.filter(user=self.request.user).filter(date=date)
-#
-#     def list(self, request, *args, **kwargs):
-#
-#
-#     def perform_create(self, serializer):
-#         serializer.save(user=self.request.user)
-#
-#
+class EntriesOnDateList(mixins.ListModelMixin,
+                        mixins.CreateModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin,
+                        generics.GenericAPIView):
+    """
+    Provides the set of entries that a User has created on a given date.
+    HTTP Methods
+        GET     : All `EntryInstance`s of a User on the date specified by the URL pattern.
+        POST    : Create a new `EntryInstance` on the given date.
+        DELETE  : Delete an `EntryInstance` on the given date.
+        PATCH   : Update an `EntryInstance` on the given date.
+    """
+    serializer_class = EntryInstanceSerializer
+
+    def get_queryset(self):
+        # A Django DateField is represented in Python as a datetime.date instance.
+        # http://www.django-rest-framework.org/api-guide/filtering/  : "Filtering agaisnt the URL"
+        date_str = self.kwargs['date']  # In the format: YYYYMMDD
+        date = datetime.date(int(date_str[:4]), int(date_str[4:6]), int(date_str[6:]))
+
+        return EntryInstance.objects.filter(user=self.request.user).filter(date=date)
+
+    def get(self, request, *args, **kwargs):
+        entry_instance_queryset = self.filter_queryset(self.get_queryset())
+        user_defined_categories_queryset = UserDefinedCategory.objects.filter(user=self.request.user)
+
+        entry_instance_serializer = self.get_serializer(entry_instance_queryset, many=True)
+        user_defined_categories_serializer = UserDefinedCategorySerializer(user_defined_categories_queryset, many=True)
+
+        data = {"EntryInstances_on_date": entry_instance_serializer.data,
+                "UserDefinedCategories": user_defined_categories_serializer.data}
+
+        return Response(data)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
 # class CategoriesList(mixins.ListModelMixin,
 #                      mixins.CreateModelMixin,
 #                      mixins.UpdateModelMixin,
