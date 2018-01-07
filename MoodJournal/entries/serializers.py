9256844b08edaff1b9755a6ffc25acc0df76934d
swpease <swpease@gmail.com>
2018-01-07 13:42:15 -0800

    Revert "beginning hyperlink model serialization"
    This reverts commit 6d41c54397512da69604f7e730757f4aff96374f.

--- MoodJournal/entries/serializers.py
+++ MoodJournal/entries/serializers.py
@@ -1,21 +1,13 @@
-from django.contrib.auth.models import User
-
 from rest_framework import serializers
 
 from .models import UserDefinedCategory
 from .models import EntryInstance
 
 
-class UserDefinedCategorySerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name='category-detail'
-    )
-    # TODO Should I just either not include it, or go with the user name?
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-
+class UserDefinedCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDefinedCategory
-        fields = ('url', 'user', 'category', 'pk',)
+        fields = ('user', 'category', 'pk',)
 
 
 class EntryInstanceSerializer(serializers.ModelSerializer):
