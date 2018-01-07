072487069340e7f52945d6cfd94dcef27f296135
swpease <swpease@gmail.com>
2018-01-07 13:53:27 -0800

    removed EntriesOnDateList view
    will just use another frontend view derived from the EntriesList
    response data.

--- MoodJournal/entries/urls.py
+++ MoodJournal/entries/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     url('^$', views.api_root),
     url('^entries/$', views.EntriesList.as_view(), name='entries-list'),
-    url('^entries/(?P<date>[0-9]{8})/$', views.EntriesOnDateList.as_view(), name='entries-on-date'),
     url('^entries/(?P<date>[0-9]{8})/(?P<category>[^/\n]+)/$', views.EntriesDetail.as_view(), name='entry-detail'),
     url('^categories/$', views.CategoriesList.as_view(), name='categories-list'),
     url('^categories/(?P<pk>[0-9]+)/$', views.CategoriesDetail.as_view(), name='category-detail')
