7a93b1217ffba2e5d153c8237e33d9f1553c75dc
swpease <swpease@gmail.com>
2018-01-22 15:36:55 -0800

    my first view
    got the Categories View setup, including CategoryWidget, and state
    conditional loading

--- MoodJournal/frontend/src/App.js
+++ MoodJournal/frontend/src/App.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
+
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
+import CategoryView from './CategoryView/CategoryView.js';
+
 
 class App extends Component {
-  onClick(e) {
-    console.log("Sending GET API call.");
+  constructor(props) {
+    super(props);
+    // Will have one state per top-level view:
+    //     - default    : welcome page
+    //     - categories : categories list
+    //     - daily      : entries for a given date
+    //     - search     : search / filter entries
+    this.state = {
+      view: 'default',
+      data: []
+    };
+    this.handleCategoriesBtnClick = this.handleCategoriesBtnClick.bind(this)
+  }
+
+  handleCategoriesBtnClick(e) {
+    axios.get('/api/categories')
+        .then(response => {
+          this.setState({
+            view: 'categories',
+            data: response.data
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
   }
 
   render() {
+    let view = null;
+    if (this.state.view == 'categories') {
+      view = (
+        <CategoryView data={this.state.data}></CategoryView>
+      )
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <button type="button" onClick={this.onClick}>
+        <button type="button" onClick={this.handleCategoriesBtnClick}>
           Send GET /categories
         </button>
+        {view}
       </div>
     );
   }
---
+++ MoodJournal/frontend/src/CategoryWidget/CategoryWidget.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class CategoryWidget extends Component {
+  render() {
+    return <li>{this.props.category}</li>;
+  }
+}
+
+
+export default CategoryWidget
