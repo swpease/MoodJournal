1157bc355c1c580273cb8c63499809a6bce6796f
swpease <swpease@gmail.com>
2018-01-31 11:42:08 -0800

    fixed icon overlap

--- MoodJournal/frontend/src/CategoryWidget/CategoryWidget.js
+++ MoodJournal/frontend/src/CategoryWidget/CategoryWidget.js
@@ -24,8 +24,6 @@ class CategoryWidget extends Component {
           <IconButton aria-label="Edit">
             <ModeEdit />
           </IconButton>
-        </ListItemSecondaryAction>
-        <ListItemSecondaryAction>
           <IconButton aria-label="Delete">
             <DeleteForever />
           </IconButton>
