fba3466581532b9ab2c1d00224cb7dc265d87d71
swpease <swpease@gmail.com>
2018-02-20 09:45:57 -0800

    cache added to settings
    https://docs.djangoproject.com/en/2.0/ref/settings/#std:setting-CACHES

--- MoodJournal/MoodJournal/settings/base.py
+++ MoodJournal/MoodJournal/settings/base.py
@@ -95,6 +95,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
