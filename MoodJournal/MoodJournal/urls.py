3d08db3a0d6de75eb60c16b963aeefd29bb96bcd
swpease <swpease@gmail.com>
2018-04-12 19:57:08 -0700

    urls update for auth / reg

--- MoodJournal/MoodJournal/urls.py
+++ MoodJournal/MoodJournal/urls.py
@@ -17,9 +17,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from rest_framework_jwt.views import refresh_jwt_token
+
 urlpatterns = [
     url(r'^api/', include('entries.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^refresh-token/', refresh_jwt_token),
     url('^$', TemplateView.as_view(template_name="index.html")),
 ]
 
