9c501aa073e26d6f14096815a66fd9a18322085f
swpease <swpease@gmail.com>
2018-04-21 16:50:52 -0700

    initial app load token fetch / refresh attempt
    will need to implement the actual login token setting before I can
    finish implementing this

--- MoodJournal/frontend/src/App.js
+++ MoodJournal/frontend/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Reboot from 'material-ui/Reboot';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import axios from 'axios';
+
+import { storageAvailable } from './Utils.js';
 
 import CategoryView from './CategoryView/CategoryView.js';
 import DailyView from './DailyView/DailyView.js';
@@ -22,8 +25,35 @@ class App extends Component {
     super(props);
     this.state = {
       view: APPVIEWS.home,
+      loggedIn: false,
     };
     this.handleTabClick = this.handleTabClick.bind(this)
+  }
+
+  componentDidMount() {
+    if(storageAvailable('localStorage')) {
+      let authToken = localStorage.getItem('authToken') || "";
+      axios.post('/refresh-token/', {token: authToken})
+        .then(
+          (response) => {
+            // axios.defaults.headers.common['Authorization'] = 'JWT' + response.data.token;
+            // this.setState({
+            //   loggedIn: true,
+            // });
+            console.log(response);
+          },
+          // I suppose I don't really care what happens with the errors here...
+          (error) => {
+            if (error.response && error.response.status === 400) {
+              console.log(error.response.data);
+            } else {
+              console.log(error);
+            }
+          }
+        );
+    } else {
+      console.log('localStorage not available. Cannot maintain session.') //TODO
+    }
   }
 
   handleTabClick(view, e) {
