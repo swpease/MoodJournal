914e0f7f925f594fcdf6a89fc153ac3ada409f46
swpease <swpease@gmail.com>
2018-04-30 15:51:23 -0700

    email verification views

--- MoodJournal/frontend/src/Auth/EmailVerification/EmailVerification.js
+++ MoodJournal/frontend/src/Auth/EmailVerification/EmailVerification.js
@@ -16,13 +16,18 @@ const theme = createMuiTheme({
 });
 
 const VERIFY_URL = '/rest-auth/registration/verify-email/'
+const VIEWS = {
+  unverified: 'unverified',
+  verified: 'verified',
+  error: 'error'
+}
 
 class EmailVerification extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      verified: false,
-      error: "",
+      view: VIEWS.unverified,
+      error: 'Your verification key has expired or is faulty. Please request a new one below.',
     }
     this.verifyEmail = this.verifyEmail.bind(this);
     this.getTokenFromUrl = this.getTokenFromUrl.bind(this);
@@ -30,7 +35,13 @@ class EmailVerification extends Component {
   }
 
   onError(error) {
-    console.log(error);
+    if (error.response.status == 404) {  // https://github.com/pennersr/django-allauth/blob/master/allauth/account/models.py from_key method: it doesn't discriminate bad or expired keys
+      this.setState({
+        view: VIEWS.error,
+      });
+    } else {
+      console.log(error);
+    }
   }
 
   verifyEmail() {
@@ -42,7 +53,7 @@ class EmailVerification extends Component {
         (response) => {
           console.log(response);
           this.setState({
-            verified: true,
+            view: VIEWS.verified,
           });
         },
         (error) => {
@@ -75,10 +86,28 @@ class EmailVerification extends Component {
         </Button>
       </React.Fragment>
     )
+    let verified = (
+      <Typography
+        variant="headline"
+        gutterBottom>
+          Email verified!
+          <a href="http://127.0.0.1:8000/">Click here</a>
+          to continue to the site.
+      </Typography>
+    )
+    let error = (
+      <Typography
+        variant="headline"
+        gutterBottom>
+          {this.state.error}
+      </Typography>
+    )
     return (
       <MuiThemeProvider theme={theme}>
         <Reboot />
-        {unverified}
+        {this.state.view === VIEWS.unverified && unverified}
+        {this.state.view === VIEWS.verified && verified}
+        {this.state.view === VIEWS.error && error}
       </MuiThemeProvider>
     );
   }
