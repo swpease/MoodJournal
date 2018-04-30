cfe4afe5284e110c2e9960849044c1bd18389f29
swpease <swpease@gmail.com>
2018-04-30 15:19:23 -0700

    axios call to verify email

--- MoodJournal/frontend/src/Auth/EmailVerification/EmailVerification.js
+++ MoodJournal/frontend/src/Auth/EmailVerification/EmailVerification.js
@@ -6,12 +6,16 @@ import Button from 'material-ui/Button';
 import axios from 'axios';
 
 
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
 const theme = createMuiTheme({
   palette: {
     type: 'dark'
   },
 });
 
+const VERIFY_URL = '/rest-auth/registration/verify-email/'
 
 class EmailVerification extends Component {
   constructor(props) {
@@ -20,6 +24,42 @@ class EmailVerification extends Component {
       verified: false,
       error: "",
     }
+    this.verifyEmail = this.verifyEmail.bind(this);
+    this.getTokenFromUrl = this.getTokenFromUrl.bind(this);
+    this.onError = this.onError.bind(this);
+  }
+
+  onError(error) {
+    console.log(error);
+  }
+
+  verifyEmail() {
+    let token = this.getTokenFromUrl();
+    axios.post(VERIFY_URL, {
+      key: token,
+    })
+      .then(
+        (response) => {
+          console.log(response);
+          this.setState({
+            verified: true,
+          });
+        },
+        (error) => {
+          if (error.response) {
+            this.onError(error);
+          } else {
+            console.log(error);
+          }
+        }
+      );
+  }
+
+  getTokenFromUrl() {
+    let url = window.location.href;
+    let wds = url.split("/");
+    let token = wds[wds.length - 2];
+    return token;
   }
 
   render() {
