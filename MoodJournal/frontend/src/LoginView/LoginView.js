9e5f2a94d83fb00902178ac4f05d7e8f15daf0c2
swpease <swpease@gmail.com>
2018-05-02 14:51:27 -0700

    password reset functionality
    this all is in LoginView. TODO: dejankify

--- MoodJournal/frontend/src/LoginView/LoginView.js
+++ MoodJournal/frontend/src/LoginView/LoginView.js
@@ -19,20 +19,31 @@ const styles = theme => ({
 });
 
 const LOGIN_URL = '/rest-auth/login/';
+const RESET_PW_URL = '/rest-auth/password/reset/';
+
+const VIEWS = {
+  default: 'default',
+  resend: 'resend',
+  resent: 'resent',
+}
 
 
 class LoginView extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      view: VIEWS.default,
       username: "",
       password: "",
+      email: "",
       generalError: "",
       userError: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.logIn = this.logIn.bind(this);
     this.onError = this.onError.bind(this);
+    this.sendPWResetEmail = this.sendPWResetEmail.bind(this);
+    this.handleForgotPW = this.handleForgotPW.bind(this);
   }
 
   handleChange(field) {
@@ -48,7 +59,8 @@ class LoginView extends Component {
     const userErrorMsgs = {
       'This field may not be blank.': 'Password required.',
       'Must include "username" and "password".': 'Username required.',
-      'Unable to log in with provided credentials.': 'Username and / or password are incorrect.'
+      'Unable to log in with provided credentials.': 'Username and / or password are incorrect.',
+      "Enter a valid email address.": "Enter a valid email address."
     }
     let userError = userErrorMsgs[userErrorMsg] || 'Unexpected Log In error. Please try again.'
     this.setState({
@@ -77,6 +89,34 @@ class LoginView extends Component {
       );
   }
 
+  sendPWResetEmail(e) {
+    axios.post(RESET_PW_URL, {
+      email: this.state.email,
+    })
+      .then(
+        (response) => {
+          console.log("email sent!")
+          this.setState({
+            view: VIEWS.resent,
+          });
+        },
+        (error) => {
+          if (error.response && error.response.status === 400) {
+            console.log(error.response);
+            this.onError(error);
+          } else {
+            this.setState({generalError: error});
+          }
+        }
+      );
+  }
+
+  handleForgotPW(e) {
+    this.setState({
+      view: VIEWS.resend,
+    });
+  }
+
   render() {
     let generalError = null;
     if (this.state.generalError) {
@@ -96,7 +136,7 @@ class LoginView extends Component {
       )
     }
 
-    return (
+    let defaultView = (
       <AuthWrapper>
         <Typography variant="title" gutterBottom>
           Log In
@@ -122,7 +162,47 @@ class LoginView extends Component {
         <Button color="primary" onClick={this.logIn}>
           {"Log In"}
         </Button>
+        <Button color="secondary" onClick={this.handleForgotPW}>
+          {"Forgot Password"}
+        </Button>
       </AuthWrapper>
+    )
+
+    let resendView = (
+      <AuthWrapper>
+        <Typography variant="title" gutterBottom>
+          Send Password Reset Email
+        </Typography>
+        {generalError}
+        {userError}
+        <TextField
+          id="email"
+          label="Email"
+          className={this.props.classes.textField}
+          value={this.state.email}
+          onChange={this.handleChange('email')}
+          margin="normal"
+        />
+        <Button color="primary" onClick={this.sendPWResetEmail}>
+          {"Send"}
+        </Button>
+      </AuthWrapper>
+    )
+
+    let resentView = (
+      <AuthWrapper>
+        <Typography variant="title" gutterBottom>
+          {"Check your email"}
+        </Typography>
+      </AuthWrapper>
+    )
+
+    return (
+      <React.Fragment>
+        {this.state.view === VIEWS.default && defaultView}
+        {this.state.view === VIEWS.resend && resendView}
+        {this.state.view === VIEWS.resent && resentView}
+      </React.Fragment>
     );
   }
 }
