b4779a036235c3908717c95a2d3be15826969652
swpease <swpease@gmail.com>
2018-05-07 21:52:49 -0700

    all entry points for the app

--- MoodJournal/frontend/config/webpack.config.prod.js
+++ MoodJournal/frontend/config/webpack.config.prod.js
@@ -59,7 +59,20 @@ module.exports = {
   // You can exclude the *.map files from the build during deployment.
   devtool: shouldUseSourceMap ? 'source-map' : false,
   // In production, we only want to load the polyfills and the app code.
-  entry: [require.resolve('./polyfills'), paths.appIndexJs],
+  entry: {
+    main: [
+      require.resolve('./polyfills'),
+      paths.appIndexJs
+    ],
+    emailverification: [
+      require.resolve('./polyfills'),
+      paths.emailverificationIndexJs,
+    ],
+    passwordreset: [
+      require.resolve('./polyfills'),
+      paths.passwordresetIndexJs,
+    ],
+  },
   output: {
     // The build folder.
     path: paths.appBuild,
