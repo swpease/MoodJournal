c1d8636f454d4a4cbb9da55ff01b93cf32a8ac93
swpease <swpease@gmail.com>
2018-05-07 21:44:44 -0700

    static files paths django

--- MoodJournal/MoodJournal/settings/production.py
+++ MoodJournal/MoodJournal/settings/production.py
@@ -3,4 +3,12 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'  # Defined in base.txt
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
