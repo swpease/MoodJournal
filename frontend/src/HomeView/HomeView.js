a362456ad20a659e30dc12cfd21ac1c2177c79f9
swpease <swpease@gmail.com>
2018-05-22 13:57:42 -0700

    HomeView skeleton

---
+++ frontend/src/HomeView/HomeView.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { withStyles } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+
+import AuthWrapper from '../AuthWrapper/AuthWrapper.js';
+
+
+function HomeView(props) {
+  return (
+    <AuthWrapper>
+      <Typography>
+        {'Welcome to Categorical Journal!'}
+      </Typography>
+      <Typography>
+        {`Categorical Journal is designed for daily reflection.
+          While it is certainly nice to write in a physical journal,
+          one of the nice things about keeping a journal in the first
+          place is being able to look back on old times and reflect
+          on where you've come from, what you've learned, or maybe
+          to prevent making the same mistake over again. With
+          Categorical Journal, you can put your journal entries
+          into any number of customizable categories, and rate the
+          entry with a happiness-level indicator ('Excellent', 'Bad',
+          etc.), then simply filter your entries history by these factors,
+          among others. Sign up to get started!`}
+      </Typography>
+    </AuthWrapper>
+  )
+}
+
+export default HomeView;
